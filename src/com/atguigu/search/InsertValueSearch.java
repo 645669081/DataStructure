9a30d8cd3dc45db360fbc08911abe29b184443e2
lw <liang645669081@126.com>
2020-03-26 07:15:56 +0800

    插值查找暂时提交

---
+++ src/com/atguigu/search/InsertValueSearch.java
@@ -0,0 +1,12 @@
+package com.atguigu.search;
+
+/**
+ * 插值查找
+ * 插值查找其实是对二分法的改进，能够找出一个更好的mid值来迅速的接近要查找数据。公式的推倒具体看数学中数值分析中的差值
+ * 具体公式是：mid = left + (right - left) * (findVal - arr[left]) / (arr[right] - arr[left])
+ */
+public class InsertValueSearch {
+    public static void main(String[] args) {
+
+    }
+}
