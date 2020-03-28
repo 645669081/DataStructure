138c92e836cbd594c5a1b397eab2f6b1ced81f2a
lw <liang645669081@126.com>
2020-03-29 01:52:11 +0800

    线索化二叉树暂时提交

---
+++ src/com/atguigu/tree/ThreadedBinaryTreeDemo.java
@@ -0,0 +1,14 @@
+package com.atguigu.tree;
+
+/**
+ * 线索化二叉树
+ * 对二叉树中有的节点可能它的left和right没有关联节点，这时候为了将这部分空间使用起来就是线索化
+ * 线索化分为前序线索化，中序线索化，后序线索化三种
+ * left指向前驱节点，right指向后继节点。具体指向哪个节点由当前的遍历模式是前序，中序，后序来决定
+ * 其本身就是为了方便节点的遍历
+ */
+public class ThreadedBinaryTreeDemo {
+    public static void main(String[] args) {
+
+    }
+}
