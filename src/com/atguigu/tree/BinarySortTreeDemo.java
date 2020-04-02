4a7da91a5bbae6c34c5c2ec4c0857cf5bd6de6f3
lw <liang645669081@126.com>
2020-04-02 11:31:28 +0800

    添加二叉排序树

--- src/com/atguigu/tree/BinarySortTreeDemo.java
+++ src/com/atguigu/tree/BinarySortTreeDemo.java
@@ -5,10 +5,41 @@ package com.atguigu.tree;
  * @Auther 梁伟
  * @Description 二叉排序树，满足小于父节点的值放左节点，大于父节点的值放右节点。中序遍历该树得到的是从小到大的排序结果，能出现这样的结果正是利用了
  * 二叉排序树插入节点时的特性，即左小右大
+ *
+ * 在对二叉排序树删除节点时又三种情况
+ * 1.是叶子节点直接删除
+ * 2.被删除节点只有一个子节点，将自己的孩子过继给父节点
+ * 3，被删除节点左右节点都有时，由排序二叉树中序遍历的特性可知该节点中序遍历下的前后节点可替代它，由此可知需要找到被删除节点左子树下的最大节点或右子树下的最小节点来替换被删除位置，
+ * 用于替换的节点的删除继续递归按照这3个条件删除即可
+ * 如二叉排序树中序遍历结果：2,7,9,15,23,45，要删除15，那么必然使用9或者23来替代它
  */
 public class BinarySortTreeDemo {
     public static void main(String[] args) {
+        int[] arr = {7, 3, 10, 12, 5, 1, 9, 2, 11};
+        BinarySortTree binarySortTree = new BinarySortTree();
+        //循环的添加结点到二叉排序树
+        for(int i = 0; i< arr.length; i++) {
+            binarySortTree.add(new Node(arr[i]));
+        }
 
+        //中序遍历二叉排序树
+        System.out.println("中序遍历二叉排序树~");
+        binarySortTree.infixOrder(); // 1, 3, 5, 7, 9, 10, 12
+
+        //测试删除节点
+        binarySortTree.delNode(10);
+
+        binarySortTree.delNode(5);
+        binarySortTree.delNode(10);
+        binarySortTree.delNode(2);
+        binarySortTree.delNode(3);
+
+        binarySortTree.delNode(9);
+        binarySortTree.delNode(1);
+        binarySortTree.delNode(7);
+
+        System.out.println("删除结点后");
+        binarySortTree.infixOrder();
     }
 }
 
@@ -30,13 +61,95 @@ class BinarySortTree {
     }
 
     /**
-     * 1. 返回的 以node 为根结点的二叉排序树的最小结点的值
-     * 2. 删除node 为根结点的二叉排序树的最小结点
+     *  获取到以node为根结点的二叉排序树的最小值节点，然后将该节点移动到node，从而完成删除节点操作。
+     *  对于二叉排序树调用该方法时注意需要传入的是被删除节点的右节点，而不是被删除节点自己
+     *  属于删除节点的第三种情况即被删除节点有左右节点
      * @param node 传入的结点(当做二叉排序树的根结点)
      * @return 返回的 以node 为根结点的二叉排序树的最小结点的值
      */
-    public int delRightTreeMin(Node node) {
-        return 0;
+    public Integer delRightTreeMin(Node node) {
+        if (node == null) {
+            return null;
+        }
+        Node targetNode = node;
+        //根据二叉排序树的特性一直走左子树就能获取到最小值
+        while (targetNode.left != null) {
+            targetNode = targetNode.left;
+        }
+        //删除找到的最小节点，因为后续要将该值移动到被删除节点
+        delNode(targetNode.value);
+        return targetNode.value;
+    }
+
+    /**
+     * delRightTreeMin相反找的是左子树的最大值，返回该值并删除该节点
+     * @param node
+     * @return
+     */
+    public Integer delLeftTreeMax(Node node) {
+        if (node == null) {
+            return null;
+        }
+        while (node.right != null) {
+            node = node.right;
+        }
+        delNode(node.value);
+        return node.value;
+    }
+
+    /**
+     * 删除结点
+     */
+    public void delNode(int value) {
+        Node delNode = search(value);
+        //要删除的节点不存在
+        if (delNode == null) {
+            return;
+        }
+        //只有找到父节点才能实现删除，即自己不能删除自己
+        Node parentNode = searchParent(value);
+        //被删除节点存在且没有父节点，说明要删除的是根结点。对根节点的删除可以分为三种情况
+        //根无子：直接置为null。根有一个子：过继节点。根有二个子，第三种方式删除。针对后两种情况其实我们后边已经做过了处理，唯一需要处理的就是根无子的情况
+        if (parentNode == null && delNode.left == null && delNode.right == null) {
+            //由于使用search方法返回的delNode对变量本身置为null是无效的，所以使用root变量来置为null
+            root = null;
+            return;
+        }
+        //第一种情况，删除叶子节点
+        if (delNode.left == null && delNode.right == null) {
+            //判断被删除节点是父节点的左节点还是右节点
+            if (parentNode.left == delNode) {
+                parentNode.left = null;
+            } else {
+                parentNode.right = null;
+            }
+        } else if (delNode.left != null && delNode.right != null) {
+            //第三种情况，被删除节点下左右子节点都有
+//            Integer rightTreeMin = delRightTreeMin(delNode.right);
+            Integer delLeftTreeMax = delLeftTreeMax(delNode.left);
+            //替换被删除节点的值
+            delNode.value = delLeftTreeMax;
+        } else {
+            //第二种情况，被删除节点下只有一个子节点。那么将被删除节点的子节点过继给父节点
+            Node inheritedNode;
+            //确定被过继的是左右中的哪个节点
+            if (delNode.left != null) {
+                inheritedNode = delNode.left;
+            } else {
+                inheritedNode = delNode.right;
+            }
+            //只有在有父节点的情况下才能过继，判断被删除节点是父节点的左节点还是右节点
+            if (parentNode != null) {
+                if (parentNode.left == delNode) {
+                    parentNode.left = inheritedNode;
+                } else {
+                    parentNode.right = inheritedNode;
+                }
+            } else {
+                //被删除节点没有父节点，说明要删除的是根结点。直接将要过继的子节点升级为新的根结点
+                root = inheritedNode;
+            }
+        }
     }
 
     /**
